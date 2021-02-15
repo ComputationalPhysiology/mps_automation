6ceb0db6ae445d2acc250ff0dfa47bb5f1126c19
Henrik Finsberg <henriknf@simula.no>
2021-02-15 21:02:13 +0100

    Add matplotlib as dependency

--- setup.py
+++ setup.py
@@ -10,13 +10,7 @@ with open("README.md") as readme_file:
 with open("HISTORY.md") as history_file:
     history = history_file.read()
 
-requirements = [
-    "openpyxl",
-    "pandas",
-    "SQLAlchemy",
-    "numpy",
-    "pyyaml",
-]
+requirements = ["openpyxl", "pandas", "SQLAlchemy", "numpy", "pyyaml", "matplotlib"]
 
 setup(
     author="Henrik Finsberg",
