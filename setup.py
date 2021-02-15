b9697addb518fcf6b00c08a8ec37e1e1f52b5667
Henrik Finsberg <henriknf@simula.no>
2021-02-15 21:31:04 +0100

    Add typer as dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,15 @@ with open("README.md") as readme_file:
 with open("HISTORY.md") as history_file:
     history = history_file.read()
 
-requirements = ["openpyxl", "pandas", "SQLAlchemy", "numpy", "pyyaml", "matplotlib"]
+requirements = [
+    "openpyxl",
+    "pandas",
+    "SQLAlchemy",
+    "numpy",
+    "pyyaml",
+    "matplotlib",
+    "typer",
+]
 
 setup(
     author="Henrik Finsberg",
